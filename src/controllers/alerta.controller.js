b37b29601607f54b401d5ab075fba5ee253f442b
Betzy <betzabet.p@gmail.com>
2023-11-29 00:26:36 -0300

    Actualizacion de lista de alertas por usuario y para admin

--- src/controllers/alerta.controller.js
+++ src/controllers/alerta.controller.js
@@ -1,12 +1,11 @@
-import { Alerta } from "../models/asociaciones.js";
+import { Alerta, Domicilio, Usuario } from "../models/asociaciones.js";
 import createError from "http-errors";
-
 
 export const crearAlerta = async (req, res, next) => {
   const { body } = req;
   try {
     const alerta = new Alerta(body);
-    alerta.UsuarioId = body.usuario.id
+    alerta.UsuarioId = body.usuario.id;
     await alerta.save();
 
     const alertaObj = alerta.toJSON();
@@ -21,103 +20,120 @@ export const crearAlerta = async (req, res, next) => {
 };
 
 export const editarAlerta = async (req, res, next) => {
-    const { id } = req.params;
-    const { body } = req;
-  
-    try {
-      const alerta = await Alerta.findByPk(id);
-      if (!alerta) {
-        throw new createError(404, "Alerta no encontrada");
-      }
-  
-      alerta.Motivo = body.Motivo;
-      alerta.UsuarioId = body.usuario.id
-      alerta.Descripcion = body.Descripcion;
-      alerta.Prioridad = body.Prioridad;
-      alerta.FechaEdicion = Date.now();
-      await alerta.save();
-  
-      const alertaEditada = alerta.toJSON();
-      res.json({
-        mensaje: "Alerta editada correctamente",
-        data: alertaEditada,
-      });
-    } catch (e) {
-      console.log(e);
-      next(e);
-    }
-  };
+  const { id } = req.params;
+  const { body } = req;
 
-  export const rechazarAlerta = async (req, res, next) => {
-    const { id } = req.params;
-    const { body } = req;
-  
-    try {
-      const alerta = await Alerta.findByPk(id);
-      if (!alerta) {
-        throw new createError(404, "Alerta no encontrada");
-      }
-  
-      alerta.Estado = 'RECHAZADA';
-      alerta.Responsable = body.usuario.id
-      alerta.Resolucion = body.Resolucion;
-      alerta.FechaEdicion = Date.now();
-      await alerta.save();
-  
-      const alertaEditada = alerta.toJSON();
-      res.json({
-        mensaje: "Alerta rechazada",
-        data: alertaEditada,
-      });
-    } catch (e) {
-      console.log(e);
-      next(e);
+  try {
+    const alerta = await Alerta.findByPk(id);
+    if (!alerta) {
+      throw new createError(404, "Alerta no encontrada");
     }
-  };
 
-  export const aprobarAlerta = async (req, res, next) => {
-    const { id } = req.params;
-    const { body } = req;
-  
-    try {
-      const alerta = await Alerta.findByPk(id);
-      if (!alerta) {
-        throw new createError(404, "Alerta no encontrada");
-      }
-  
-      alerta.Estado = 'APROBADA';
-      alerta.Responsable = body.usuario.id
-      alerta.FechaEdicion = Date.now();
-      await alerta.save();
-  
-      const alertaEditada = alerta.toJSON();
-      res.json({
-        mensaje: "Alerta aprobada correctamente",
-        data: alertaEditada,
-      });
-    } catch (e) {
-      console.log(e);
-      next(e);
+    alerta.Motivo = body.Motivo;
+    alerta.UsuarioId = body.usuario.id;
+    alerta.Descripcion = body.Descripcion;
+    alerta.Prioridad = body.Prioridad;
+    alerta.FechaEdicion = Date.now();
+    await alerta.save();
+
+    const alertaEditada = alerta.toJSON();
+    res.json({
+      mensaje: "Alerta editada correctamente",
+      data: alertaEditada,
+    });
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+};
+
+export const rechazarAlerta = async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  try {
+    const alerta = await Alerta.findByPk(id);
+    if (!alerta) {
+      throw new createError(404, "Alerta no encontrada");
     }
-  };
 
-  export const listarAlertas = async (req, res, next) => {
-    try {
-      const alerta = await Alerta.findAll({
-        order: [
-          ['FechaCreacion', 'DESC']
-        ]
-      });
-      if (!alerta) {
-        throw new createError(404, "Alertas no encontrados");
-      }
-      res.json({
-        mensaje: "Alertas obtenidas",
-        data: alerta,
-      });
-    } catch (e) {
-      next(e);
+    alerta.Estado = "RECHAZADA";
+    alerta.Responsable = body.usuario.id;
+    alerta.Resolucion = body.Resolucion;
+    alerta.FechaEdicion = Date.now();
+    await alerta.save();
+
+    const alertaEditada = alerta.toJSON();
+    res.json({
+      mensaje: "Alerta rechazada",
+      data: alertaEditada,
+    });
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+};
+
+export const aprobarAlerta = async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  try {
+    const alerta = await Alerta.findByPk(id);
+    if (!alerta) {
+      throw new createError(404, "Alerta no encontrada");
     }
-  };
 
-  
+    alerta.Estado = "APROBADA";
+    alerta.Responsable = body.usuario.id;
+    alerta.FechaEdicion = Date.now();
+    await alerta.save();
+
+    const alertaEditada = alerta.toJSON();
+    res.json({
+      mensaje: "Alerta aprobada correctamente",
+      data: alertaEditada,
+    });
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+};
+
+export const listarAlertas = async (req, res, next) => {
+  const { usuario } = req.body;
+  s;
+  try {
+    let condicionesDeBusqueda = {};
+
+    if (usuario.rol !== "ADMIN") {
+      condicionesDeBusqueda = {
+        where: { UsuarioId: usuario.id },
+      };
+    } else {
+      condicionesDeBusqueda = {
+        include: [
+          {
+            model: Usuario,
+            include: [{ model: Domicilio }],
+          },
+        ],
+      };
+    }
+
+    const alerta = await Alerta.findAll({
+      ...condicionesDeBusqueda,
+      order: [["FechaCreacion", "DESC"]],
+    });
+
+    if (!alerta) {
+      throw new createError(404, "Alertas no encontrados");
+    }
+    res.json({
+      mensaje: "Alertas obtenidas",
+      data: alerta,
+    });
+  } catch (e) {
+    next(e);
+  }
+};
